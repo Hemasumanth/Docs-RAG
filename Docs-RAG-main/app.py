17b46c77dfac8894dfdd10086bd24f8324f3e9f5
Hema sumanth Rasineni <118580548+Hemasumanth@users.noreply.github.com>
2025-01-26 16:22:29 -0500

    Update app.py

--- Docs-RAG-main/app.py
+++ Docs-RAG-main/app.py
@@ -24,37 +24,3 @@ system_content = """Answer the query purely using the context provided. But, if 
                     For questions related to API reference, first look at the API Reference not the examples in the context.
                     Do not try to make things up. Be succinct."""
 
-agent = QueryAgent(
-    embedding_model_name="thenlper/gte-large",
-    llm="mistralai/Mixtral-8x7B-Instruct-v0.1",
-    max_context_length=32768,
-    system_content=system_content
-)
-
-# Session State Check
-if "messages" not in st.session_state:
-    st.session_state['messages'] = []
-
-# Display Messages
-for message in st.session_state['messages']:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# Function to Stream Responses
-def stream_responses():
-    result = agent(query=st.session_state['messages'][-1]['content'], stream=True, num_chunks=7)
-    for content in result['answer']:
-        yield content
-    yield "\n\nRelated Sources:\n"
-    for i, source in enumerate(result['sources']):
-        yield f"{i+1}. {source}\n"
-
-# User Input
-prompt = st.chat_input("Hi, I'm your AI assistant to help you in answering Scikit-Learn related queries. Ask me anything!")
-if prompt:
-    st.session_state['messages'].append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    with st.chat_message("assistant"):
-        message = st.write_stream(stream_responses())
-        st.session_state['messages'].append({"role": "assistant", "content": message})
